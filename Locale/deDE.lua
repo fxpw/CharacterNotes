e689216533f701308a69ec41ada098de1265485a
fxpw <fxpw@outlook.com>
2023-01-11 12:48:25 +0300

    first commit

---
+++ Locale/deDE.lua
@@ -0,0 +1,37 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CharacterNotes", "deDE", false)
+
+if not L then return end
+
+L["Are you sure you wish to delete the note for:"] = "Bist du sicher dass du die Notiz löschen willst für:" -- Needs review
+L["Cancel"] = "Abbrechen"
+L["Character Name"] = "Charaktername"
+L["Character Notes"] = "Charakternotizen"
+L["Clear"] = "Löschen"
+L["Close"] = "Schließen"
+L["Delete"] = "Löschen"
+L["Deleted note for %s"] = "Notiz für %s gelöscht"
+L["Delete Note"] = "Notiz löschen"
+L["Edit"] = "Editieren"
+L["Edit Note"] = "Notiz editieren" -- Needs review
+L["Left click"] = "Linksklick" -- Needs review
+L["Lock"] = "Sperren" -- Needs review
+L["Minimap Button"] = "Minimap-Symbol" -- Needs review
+L["No note found for "] = "Keine Notiz gefunden für " -- Needs review
+L["Note"] = "Notiz" -- Needs review
+L["Note: "] = "Notiz: " -- Needs review
+L["Notes for Party and Raid Members"] = "Notizen für Gruppen- und Schlachtzugsmitglieder" -- Needs review
+L["Party Members"] = "Gruppenmitglieder" -- Needs review
+L["Raid Members"] = "Schlachtzugmitglieder"
+L["Remember Position"] = "Position merken" -- Needs review
+L["Right click"] = "Rechtsklick"
+L["Save"] = "Speichern"
+L["Search"] = "Suchen"
+L["Set note for %s: %s"] = "Notiz festlegen für %s: %s" -- Needs review
+L["Show notes at logon"] = "Notizen beim Login anzeigen"
+L["Show notes in tooltips"] = "Notizen in Tooltips anzeigen"
+L["Show notes with who results"] = "Notizen bei /wer-Abragen anzeigen"
+L["Toggle the minimap button"] = "Minimap-Symbol ein/ausblenden"
+L["Tooltip Options"] = "Tooltip-Optionen"
+L["to open/close the configuration."] = "um die Konfigurationen zu öffnen/schliessen."
+L["to open/close the window"] = "um das Fenster zu öffnen/schliessen."
+
---
+++ Locale/ruRU.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("CharacterNotes", "ruRU", false)
+
+if not L then return end
+
+
